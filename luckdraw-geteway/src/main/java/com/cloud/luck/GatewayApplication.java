8e0c3e7998a0c8b8e828f32315bfb1578d8c758a
qierkang <Qierkang123>
2020-01-29 23:47:43 +0800

    2020年01月29日23:44:11 更新配置

--- luckdraw-geteway/src/main/java/com/cloud/luck/GatewayApplication.java
+++ luckdraw-geteway/src/main/java/com/cloud/luck/GatewayApplication.java
@@ -1,5 +1,11 @@
 package com.cloud.luck;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+import com.cloud.luck.config.TestConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,6 +14,8 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
 
 /**
  * *********************************************************
@@ -28,6 +36,17 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 public class GatewayApplication {
 
+    private static final Log log = LogFactory.get();
+
+    /**
+     * null ：作用为在取不到对应配置值时，采用默认值null赋值
+     */
+    @Value("${luck.mes.name:#{null}}")
+    private String title;
+
+    @Autowired
+    private TestConfig testConfig;
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
@@ -36,5 +55,24 @@ public class GatewayApplication {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * Description: [ spring boot 启动初始化 ]
+     *
+     * @param
+     * @return void
+     * @Author <a href="mailto:xyqierkang@gmail.com">ErKang Qi</a>
+     * @Date Created in 2020-01-05 03:13
+     * <p>File:  </p>
+     */
+    @PostConstruct
+    private void init() {
+        try {
+            log.info("#初始化配置中心 [{}]操作时间[{}]", title, DateUtil.now());
+            log.info("#初始化YML [{}]操作时间[{}]", testConfig.getName(), DateUtil.now());
+        } catch (Exception e) {
+            log.error("#初始化配置错误！");
+        }
     }
 }
